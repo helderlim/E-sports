5587fa17693310d75db96bbba3eb6e39aa31b340
helderlim <helderfelima16@gmail.com>
2022-09-16 01:06:06 -0300

    feat: separa componentes e consome minha api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,28 @@
 import './styles/main.css';
-
 import logoImg from './assets/logo-nlw-esports.svg'
+import { GameBanner } from './components/GameBanner';
+import { useState, useEffect } from 'react';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
-import { MagnifyingGlassPlus } from 'phosphor-react'
-
-const games = [
-  {imgUrl: 'game-1.png', title: 'League of Legends', description: '4 anúncios'},
-  {imgUrl: 'game-2.png', title: 'Apex Legends', description: '4 anúncios'},
-  {imgUrl: 'game-3.png', title: 'COD Warzone', description: '4 anúncios'},
-  {imgUrl: 'game-4.png', title: 'COD Warzone', description: '4 anúncios'},
-  {imgUrl: 'game-5.png', title: 'COD Warzone', description: '4 anúncios'},
-  {imgUrl: 'game-6.png', title: 'COD Warzone', description: '4 anúncios'},
-]
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number
+  }
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(()=> {
+    fetch('http://localhost:3333/games').then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setGames(data)
+    })
+  }, [])
   return(
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="Logo esports" />
@@ -23,31 +32,19 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        {games.map(image => {
+        {games.map(game => {
           return (
-          <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src={image.imgUrl} alt="" />
-            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>{image.title}</strong>
-              <span className='text-zinc-300 text-sm block'>{image.description}</span>
-            </div>
-          </a>
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
           )
         })}
       </div>
 
-      <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2a2634] px-8 py-6 flex justify-between items-center'>
-          <div className=''>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encotrar novos players!</span>
-          </div>
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner/>
     </div>
   )
 }
